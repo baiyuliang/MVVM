0a97fe47dbc90108803d9f5fd6b4be0a684e903f
白玉梁 <453503875@qq.com>
2020-06-05 10:27:00 +0800

    简化接口调用

--- app/src/main/java/com/byl/mvvm/api/HttpUtil.kt
+++ app/src/main/java/com/byl/mvvm/api/HttpUtil.kt
@@ -6,9 +6,9 @@ class HttpUtil {
 
     private val mService by lazy { RetrofitClient.getInstance().create() }
 
-    suspend fun test(options: LinkedHashMap<String, String?>) = mService.test(options)
+    //suspend fun test(options: LinkedHashMap<String, String?>) = mService.test(options)
 
-    suspend fun getArticleList(page: Int) = mService.getArticleList(page)
+    //suspend fun getArticleList(page: Int) = mService.getArticleList(page)
 
 
     companion object {
@@ -18,6 +18,11 @@ class HttpUtil {
         fun getInstance() = httpUtil ?: synchronized(this) {
             httpUtil ?: HttpUtil().also { httpUtil = it }
         }
+    }
+
+    //可以直接在BaseViewModel中获取取ApiService对象，简化接口调用
+    fun getService(): ApiService {
+        return mService
     }
 
 }
