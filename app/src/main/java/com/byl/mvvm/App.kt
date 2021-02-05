1d24e6614639d3de59a7a65df4bfa47e455629f0
lishide <lishide@ebaolife.com>
2021-02-05 10:40:29 +0800

    update: 升级刷新框架到 2.0.1

--- app/src/main/java/com/byl/mvvm/App.kt
+++ app/src/main/java/com/byl/mvvm/App.kt
@@ -1,13 +1,10 @@
 package com.byl.mvvm
 
 import android.app.Application
-import android.content.Context
 import com.byl.mvvm.util.Logg
-import com.scwang.smartrefresh.layout.SmartRefreshLayout
-import com.scwang.smartrefresh.layout.api.RefreshLayout
-import com.scwang.smartrefresh.layout.constant.SpinnerStyle
-import com.scwang.smartrefresh.layout.footer.ClassicsFooter
-import com.scwang.smartrefresh.layout.header.ClassicsHeader
+import com.scwang.smart.refresh.footer.ClassicsFooter
+import com.scwang.smart.refresh.header.ClassicsHeader
+import com.scwang.smart.refresh.layout.SmartRefreshLayout
 
 
 class App : Application() {
@@ -28,31 +25,17 @@ class App : Application() {
 
     init {
         //设置全局默认配置（优先级最低，会被其他设置覆盖）
-        SmartRefreshLayout.setDefaultRefreshInitializer { _: Context?, layout: RefreshLayout ->
-            //开始设置全局的基本参数
-            layout.setFooterHeight(40f)
-            layout.setDisableContentWhenLoading(false)
-            layout.setDisableContentWhenRefresh(true) //是否在刷新的时候禁止列表的操作
-            layout.setDisableContentWhenLoading(true) //是否在加载的时候禁止列表的操作
-            layout.setEnableOverScrollBounce(false)
+        SmartRefreshLayout.setDefaultRefreshInitializer { _, layout ->
+            layout.setEnableLoadMore(true)
+            layout.setEnableLoadMoreWhenContentNotFull(false)
+            layout.setDisableContentWhenRefresh(true)
+            layout.setDisableContentWhenLoading(true)
         }
-        SmartRefreshLayout.setDefaultRefreshHeaderCreator { context: Context?, _: RefreshLayout? ->
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, _ ->
             ClassicsHeader(context)
-                    .setSpinnerStyle(SpinnerStyle.Translate)
-                    .setTextSizeTitle(13f)
-                    .setDrawableArrowSize(15f)
-                    .setDrawableProgressSize(15f)
-                    .setDrawableMarginRight(10f)
-                    .setFinishDuration(0)
         }
-        SmartRefreshLayout.setDefaultRefreshFooterCreator { context: Context?, _: RefreshLayout? ->
+        SmartRefreshLayout.setDefaultRefreshFooterCreator { context, _ ->
             ClassicsFooter(context)
-                    .setSpinnerStyle(SpinnerStyle.Translate)
-                    .setTextSizeTitle(13f)
-                    .setDrawableArrowSize(15f)
-                    .setDrawableProgressSize(15f)
-                    .setDrawableMarginRight(10f)
-                    .setFinishDuration(0)
         }
     }
 
